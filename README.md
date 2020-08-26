da281940faca51bd7dd082ff3556f8015579f16a
InoveAlumnos <64550682+InoveAlumnos@users.noreply.github.com>
2020-08-26 17:56:22 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,43 @@
+![Inove banner](img/inove.jpg)
+Inove Escuela de Código\
+info@inove.com.ar\
+Web: [Inove](http://inove.com.ar)
+
+# Primeros pasos en JavaScript
+__Los objetivos principales de esta clase son:__
+
+1. Entender la sintaxis del lenguaje.
+2. Identificar fallas en la sintaxis.
+3. Aprender a utilizar las herramientas.
+4. Entender qué es una variable.
+5. Realizar operaciones básicas con variables.
+6. Depurar el programa.
+
+__En el repositorio encontraran__:
+* Un archivo script.js con el código visto en clase
+* Un archivo con la tarea para hacer en casa, llamado ejercitacion.js,
+se debe generar el link en el archivo index.html para enlazarlo.
+
+### Nota importante:
+Los ejercicios en el script ejercitacion.js, están comentados, de manera que deben ir "descomentandolos" cuando sea necesario. Los comentarios estan puestos de esta manera:
+
+```
+/* ELIMINE ESTE COMENTARIO
+var textoNumero = undefined; // <-- aquí está el código del ejercicio en un comentario largo
+console.log("etc");
+*/
+```
+
+Se debe quitar los caracteres de comentario largo /*  */ para poder realizar el ejercicio:
+
+```
+var textoNumero = undefined; // <-- aquí está el código del ejercicio sin el comentario largo
+console.log("etc");
+```
+Ante cualquier inquietud, comuniquese con el profesor  por medio del campus.
+
+##### Éxitos!
+
+----------------------------
+# Consultas
+alumnos@inove.com.ar
---
+++ js/ejercitacion.js
@@ -0,0 +1,80 @@
+"use strict";
+
+/* Tarea
+ * Objetivos: adquirir herramientas para identificar 
+ * problemas en el código, identificando si se producen
+ * errores de sintaxis o de lógica.
+ * debe descomentar cada ejercicio!
+ */
+
+// 1. Corrija los errores presentes en el código:
+
+/* ELIMINE ESTE COMENTARIO
+var nom bre = " otros";
+var accion "Programamos";
+vr negacion = ", no ";
+para " para "
+quien = "nosotros mismos"
+
+console . log(accion + para + nombre + negacion + para + quien);
+*/
+
+//-------------------------------------------------------------------
+// 2. Realice las modificaciones en el código para que la operación
+//    entregue como resultado 10:
+
+/* ELIMINE ESTE COMENTARIO
+var numera dor = 100;
+var denominador10;
+cociente
+cociente = numerador / denominador;
+
+console.log(cociente;
+*/
+
+//-------------------------------------------------------------------
+// 3. El siguiente codigo tiene un error, de qué se trata?
+//    proponga una alternativa para que se imprima 1 en consola, 
+//    manteniendo las operaciones de división.
+
+/* ELIMINE ESTE COMENTARIO
+var numerador = 25;
+var denominador = 25 - numerador;
+var resultado = numerador/denominador;
+
+console.log(resultado/resultado);
+*/
+
+//-------------------------------------------------------------------
+// 4. Realice un acumulador utilizando una variable, debe acumular en 5 lineas
+//    de código el numero 100, comenzando desde el 1.
+
+/* ELIMINE ESTE COMENTARIO
+var acumulador = 1; //No utilizar mas variables ;D
+//primer paso:
+
+//segundo paso:
+
+//tercer paso:
+
+//cuarto paso:
+
+//quinto paso:
+
+//Imprimimos en consola:
+console.log("El resultado del programa es: "+ acumulador); 
+// El resultado debe ser 100
+*/
+
+//-------------------------------------------------------------------
+// 5. Utilizar una sola variable para almacenar texto o numeros, 
+//    e imprimir en consola varias veces para construir la frase:
+//    El numero
+//    7
+//    me trae mas suerte que el numero 
+//    13
+
+/* ELIMINE ESTE COMENTARIO
+var textoNumero = undefined;
+*/
+
